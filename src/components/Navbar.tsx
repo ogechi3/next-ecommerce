8dd9b4923cd917f561aab24e1c1df63fd82a6f49
Thomas Jennifer <jsamuelt13@gmail.com>
2025-02-08 04:51:22 +0100

    add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,17 +18,24 @@ const Navbar = () => {
       {/* BIGGER SCREENS */}
       <div className="hidden md:flex items-center h-full justify-between gap-8">
         {/* LEFT */}
-        <div className="w-1/3">
-          <Link href="/" className='flex gap-3 items-center'>
-          <Image src="/logo.png" alt='logo img' width={24} height={24}/>
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+          <Link href="/" className="flex gap-3 items-center">
+            <Image src="/logo.png" alt="logo img" width={24} height={24} />
             <div className="text-2xl tracking-wide">LAMA</div>
           </Link>
+          <div className='hidden xl:flex gap-4'>
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
 
         {/* RIGHT */}
-        <div className="w-2/3 flex items-center justify-between gap-8">
-        <SearchBar/>
-        <NavbarIcons/>
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavbarIcons />
         </div>
       </div>
     </div>
